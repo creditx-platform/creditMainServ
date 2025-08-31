74516515dac32f98a4e6bd95ac3e24e3517940e2
AkshayKappala <akshaykappala98@gmail.com>
2025-08-31 11:17:20 -0500

    Remove unused Mockito argument matcher import in HoldEventServiceTest

--- src/test/java/com/creditx/main/HoldEventServiceTest.java
+++ src/test/java/com/creditx/main/HoldEventServiceTest.java
@@ -2,7 +2,6 @@ package com.creditx.main;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
